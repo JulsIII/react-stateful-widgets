1989981ae9929297e0e66d6f29a0b51282b1ae4b
Your Name <you@example.com>
2020-12-08 00:54:10 -0600

    6 - Correction & Backup

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -28,8 +28,8 @@ export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
 
-  const [names] = useState(listOfAwesome);
-  const [id, setId] = useState("null");
+  const [names, setNames] = useState(listOfAwesome);
+  const [pid, setId] = useState("null");
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -37,13 +37,13 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    return names[id-1].name;
+    return names[pid-1].name
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: (id != "null" ? 'gold' : 'royalblue'), // 🤔 color turns to gold, when celebrating
+    color: (pid >= 0) ? 'gold' : 'royalblue' // 🤔 color turns to gold, when celebrating
   };
 
   return (
@@ -56,7 +56,7 @@ export default function Programmers() {
           we could never add or edit programmers in the future. The list would be a static thing." */
           names.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => {setId(dev.name)}}>Feature</button>
+              {dev.name} <button onClick={() => { setId(dev.id) }}>Feature</button>
             </div>
           )
         }
@@ -66,7 +66,8 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          id != "null" ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
+          (pid >= 0)
+          ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
       </div>
